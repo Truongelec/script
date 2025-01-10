eb1055d8add756abc9090dee2a0630d25867b69d
Truongelec <103218312+Truongelec@users.noreply.github.com>
2025-01-10 08:56:07 +0700

    Add files via upload

---
+++ scirpt_linhtinh.sh
@@ -0,0 +1,13 @@
+            Windown 
+
+```
+w32tm /config /manualpeerlist:103.200.22.11 /syncfromflags:manual /reliable:YES /update
+``
+
+            code git 
+00acb660-ec24-42ff-b384-82a2688e7c6f
+
+
+
+================= www and non-wwww
+- cấu hình trong hyperlink wp-admin 
